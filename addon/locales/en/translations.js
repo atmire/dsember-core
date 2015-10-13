8bad8b0ccd359a88b6ff8f40e0e2daaf14815779
Art Lowel <art@ArtMac.local>
2015-10-13 17:56:19 +0200

    added i18n

---
+++ addon/locales/en/translations.js
@@ -0,0 +1,19 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+
+  "item": {
+    "detailed": {
+      "key": "Key",
+      "value": "Value",
+      "language": "Lang"
+    }
+  }
+};

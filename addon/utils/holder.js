e3ab755d163b88b5f356103286748317d11df990
Art Lowel <art@ArtMac.local>
2015-10-15 17:15:27 +0200

    fixed collection and community serializers/models and added holderjs for thumbnails

---
+++ addon/utils/holder.js
@@ -0,0 +1,7 @@
+function run() {
+  return window.Holder.run.apply(this, arguments);
+}
+
+export {
+  run
+};
---
+++ tests/unit/utils/holder-test.js
@@ -0,0 +1,10 @@
+import holder from '../../../utils/holder';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | holder');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = holder();
+  assert.ok(result);
+});
